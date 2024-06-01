1c93e78229a9218da56ab87fc50d9d60609c5167
sahatovbatyr <sahatovbatyr@gmail.com>
2024-06-01 12:09:52 +0300

    spring initializer project

---
+++ src/test/java/com/todo_list/todo_list_my_artifact/TodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todo_list.todo_list_my_artifact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
