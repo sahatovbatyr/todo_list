1513d6d6ef02414d8f1f60dfeece9743ddc32327
sahatovbatyr <sahatovbatyr@gmail.com>
2024-06-02 18:06:16 +0300

    DataInitializer and EntityNotFoundByIdException

--- src/main/java/com/todo_list/todo_list_my_artifact/exceptions/EntityNotFoundByIdException.java
+++ src/main/java/com/todo_list/todo_list_my_artifact/exceptions/EntityNotFoundByIdException.java
@@ -1,4 +1,8 @@
 package com.todo_list.todo_list_my_artifact.exceptions;
 
-public class EntityNotFoundByIdException {
+public class EntityNotFoundByIdException extends RuntimeException{
+
+    public EntityNotFoundByIdException( String message) {
+        super(message);
+    }
 }
--- src/main/java/com/todo_list/todo_list_my_artifact/initializer/DataInitializer.java
+++ src/main/java/com/todo_list/todo_list_my_artifact/initializer/DataInitializer.java
@@ -1,2 +1,30 @@
-package com.todo_list.todo_list_my_artifact.initializer;public class DataInitializer {
+package com.todo_list.todo_list_my_artifact.initializer;
+
+import com.todo_list.todo_list_my_artifact.dao.UserRoleDao;
+import com.todo_list.todo_list_my_artifact.models.RoleType;
+import com.todo_list.todo_list_my_artifact.models.UserRole;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataInitializer implements ApplicationRunner {
+
+    @Autowired
+    UserRoleDao userRoleDao;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+        if( userRoleDao.count()==0) {
+            for(RoleType roleType: RoleType.values()) {
+
+                RoleType role = RoleType.getOne_byDisplayName(roleType.getDisplayeName());
+                userRoleDao.save( new UserRole( role ) );
+            }
+
+        }
+
+    }
 }
