435af00c836bf247327333612a54d956fdada40e
sahatovbatyr <sahatovbatyr@gmail.com>
2024-06-08 11:08:38 +0300

    init class to add default data to db

--- src/main/java/com/todo_list/todo_list_my_artifact/initializer/DataInitializer.java
+++ src/main/java/com/todo_list/todo_list_my_artifact/initializer/DataInitializer.java
@@ -1,30 +1,49 @@
 package com.todo_list.todo_list_my_artifact.initializer;
 
-import com.todo_list.todo_list_my_artifact.dao.UserRoleDao;
+import com.todo_list.todo_list_my_artifact.config.YamlConfig;
 import com.todo_list.todo_list_my_artifact.models.RoleType;
+
 import com.todo_list.todo_list_my_artifact.models.UserRole;
+import com.todo_list.todo_list_my_artifact.services.UserRoleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.stream.Collectors;
+
 @Component
 public class DataInitializer implements ApplicationRunner {
 
     @Autowired
-    UserRoleDao userRoleDao;
+    private YamlConfig yamlConfig;
+
+    @Autowired
+    UserRoleService userRoleService;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
 
-        if( userRoleDao.count()==0) {
-            for(RoleType roleType: RoleType.values()) {
 
-                RoleType role = RoleType.getOne_byDisplayName(roleType.getDisplayeName());
-                userRoleDao.save( new UserRole( role ) );
+        System.out.println("YAML Properties " + yamlConfig.toString());
+
+        if(userRoleService.getAll().isEmpty()) {
+            for(RoleType iRoleType: RoleType.values()) {
+
+                UserRole newRole = new UserRole();
+                newRole.setRoleType( iRoleType );
+
+                userRoleService.create( newRole  );
             }
 
+//            Arrays.stream(RoleType.values())
+//                    .map( r-> { var u= new UserRole( ); u.setRoleType(r); return u;  } )
+//                    .collect(Collectors.toList());
+
         }
+
 
     }
 }
