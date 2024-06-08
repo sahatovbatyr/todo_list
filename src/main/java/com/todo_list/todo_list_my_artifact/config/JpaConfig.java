5077bdffeecba83565f78c4b20632569b61f0d7c
sahatovbatyr <sahatovbatyr@gmail.com>
2024-06-08 11:09:36 +0300

    jpa config

--- src/main/java/com/todo_list/todo_list_my_artifact/config/JpaConfig.java
+++ src/main/java/com/todo_list/todo_list_my_artifact/config/JpaConfig.java
@@ -1,4 +1,18 @@
 package com.todo_list.todo_list_my_artifact.config;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+@ComponentScan(basePackages = "com.todo_list" )
+@EnableJpaRepositories(basePackages = "com.todo_list.todo_list_my_artifact.dao"
+//        includeFilters = {
+//                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = UserDao.class),
+//                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = UserRoleTempl.class)
+//
+//        }
+)
 public class JpaConfig {
+
 }
