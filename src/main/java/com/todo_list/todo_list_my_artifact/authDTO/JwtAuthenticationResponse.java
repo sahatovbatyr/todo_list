f5e094a7fa879587defe609329ddd57dc24a8a0a
sahatovbatyr <sahatovbatyr@gmail.com>
2024-06-08 11:10:39 +0300

    added security classes

--- src/main/java/com/todo_list/todo_list_my_artifact/authDTO/JwtAuthenticationResponse.java
+++ src/main/java/com/todo_list/todo_list_my_artifact/authDTO/JwtAuthenticationResponse.java
@@ -1,2 +1,17 @@
-package com.todo_list.todo_list_my_artifact.authDTO;public class JwtAuthenticationResponse {
+package com.todo_list.todo_list_my_artifact.authDTO;
+
+//import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+//@Schema(description = "Ответ c токеном доступа")
+public class JwtAuthenticationResponse {
+//    @Schema(description = "Токен доступа", example = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsImV4cCI6MTYyMjUwNj...")
+    private String token;
 }
