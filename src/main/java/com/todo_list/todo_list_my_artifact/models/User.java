3d01720137852fc7fe3b0c8dfcd312a897f3b9dd
sahatovbatyr <sahatovbatyr@gmail.com>
2024-06-16 11:53:17 +0300

    remove UserDetails from User

--- src/main/java/com/todo_list/todo_list_my_artifact/models/User.java
+++ src/main/java/com/todo_list/todo_list_my_artifact/models/User.java
@@ -20,8 +20,8 @@ import java.util.stream.Collectors;
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "users" )
-public class User implements UserDetails {
-
+public class User {
+//implements UserDetails
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -53,36 +53,36 @@ public class User implements UserDetails {
 //    private List<Task> assignedTasks = new ArrayList<>();
 
 
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        List<SimpleGrantedAuthority> authorities;
-
-        authorities = getRoles().stream()
-                .map( r-> new SimpleGrantedAuthority( r.getRoletype().name() ) )
-                .collect(Collectors.toList());
-        return authorities;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
+//
+//    @Override
+//    public Collection<? extends GrantedAuthority> getAuthorities() {
+//        List<SimpleGrantedAuthority> authorities;
+//
+//        authorities = getRoles().stream()
+//                .map( r-> new SimpleGrantedAuthority( r.getRoletype().name() ) )
+//                .collect(Collectors.toList());
+//        return authorities;
+//    }
+//
+//    @Override
+//    public boolean isAccountNonExpired() {
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean isAccountNonLocked() {
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean isCredentialsNonExpired() {
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean isEnabled() {
+//        return true;
+//    }
 
 
     public String toString() {
