e206ee0e17ff54ae2384a7fec10b159a347d06aa
sahatovbatyr <sahatovbatyr@gmail.com>
2024-06-16 11:54:46 +0300

    create app Prop's reading  class

--- src/main/java/com/todo_list/todo_list_my_artifact/props/JwtProperties.java
+++ src/main/java/com/todo_list/todo_list_my_artifact/props/JwtProperties.java
@@ -1,4 +1,19 @@
 package com.todo_list.todo_list_my_artifact.props;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+
+@Component
+@Getter
+@Setter
+@ConfigurationProperties(prefix = "token.jwt")
 public class JwtProperties {
+    private String secret;
+    private Duration accessPeriod;
+    private Duration refreshPeriod;
 }
