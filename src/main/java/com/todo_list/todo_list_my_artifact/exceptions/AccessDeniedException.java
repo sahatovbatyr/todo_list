0b93d7ac11139ae4ffccf826d2d2cfb7ca31f9ea
sahatovbatyr <sahatovbatyr@gmail.com>
2024-06-21 11:23:21 +0300

    fixed AccessDeniedException and JwtAthenticateException

--- src/main/java/com/todo_list/todo_list_my_artifact/exceptions/AccessDeniedException.java
+++ src/main/java/com/todo_list/todo_list_my_artifact/exceptions/AccessDeniedException.java
@@ -1,4 +1,10 @@
 package com.todo_list.todo_list_my_artifact.exceptions;
 
-public class AccessDeniedException {
+public class AccessDeniedException extends RuntimeException {
+
+    public AccessDeniedException(String message){
+
+        super( message);
+
+    }
 }
--- src/main/java/com/todo_list/todo_list_my_artifact/exceptions/JwtAuthenticationException.java
+++ src/main/java/com/todo_list/todo_list_my_artifact/exceptions/JwtAuthenticationException.java
@@ -4,4 +4,8 @@ public class JwtAuthenticationException extends RuntimeException {
     public JwtAuthenticationException(String message) {
         super(message);
     }
+
+    public JwtAuthenticationException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
