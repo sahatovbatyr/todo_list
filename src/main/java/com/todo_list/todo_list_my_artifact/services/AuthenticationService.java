5de9fd2b8b401e49df9068076b833b4c34712f6e
sahatovbatyr <sahatovbatyr@gmail.com>
2024-06-23 19:00:55 +0300

    cleaned up the  code

--- src/main/java/com/todo_list/todo_list_my_artifact/services/AuthenticationService.java
+++ src/main/java/com/todo_list/todo_list_my_artifact/services/AuthenticationService.java
@@ -3,8 +3,9 @@ package com.todo_list.todo_list_my_artifact.services;
 import com.todo_list.todo_list_my_artifact.authDTO.JwtRequestDto;
 import com.todo_list.todo_list_my_artifact.authDTO.JwtResponseDto;
 import com.todo_list.todo_list_my_artifact.authDTO.SignUpRequestDto;
-import com.todo_list.todo_list_my_artifact.exceptions.JwtAuthenticationException;
+import com.todo_list.todo_list_my_artifact.exceptions.JwtAuthFailException;
 import com.todo_list.todo_list_my_artifact.models.User;
+import com.todo_list.todo_list_my_artifact.security.JwtEntity;
 import com.todo_list.todo_list_my_artifact.security.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.apache.logging.log4j.LogManager;
@@ -12,7 +13,9 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 
@@ -81,10 +84,13 @@ public class AuthenticationService {
 
             User user = userService.findByUsername(request.getUsername());
 
-            String accessToken= jwtTokenService
-                    .createAccessToken( user.getId(), user.getUsername(), user.getRoles());
-            String refreshToken = jwtTokenService
-                    .createRefreshToken(user.getId(), user.getUsername());
+            String accessToken= jwtTokenService.createAccessToken(
+                    user.getId(), user.getUsername(), user.getRoles()
+            );
+
+            String refreshToken = jwtTokenService.createRefreshToken(
+                    user.getId(), user.getUsername()
+            );
             jwtResponse.setId(user.getId());
             jwtResponse.setUsername(user.getUsername());
             jwtResponse.setAccessToken( accessToken );
@@ -94,14 +100,14 @@ public class AuthenticationService {
 
 
         } catch ( BadCredentialsException ex) {
-            throw  new JwtAuthenticationException("Authentication failed.Username or password not found. "
+            throw  new JwtAuthFailException("Authentication failed.Username or password not found. "
                     +ex.getMessage());
         }
         catch ( AuthenticationException ex) {
-           throw  new JwtAuthenticationException("Authentication failed." +ex.getMessage());
+           throw  new JwtAuthFailException("Authentication failed." +ex.getMessage());
 
         } catch ( Exception ex) {
-            throw  new JwtAuthenticationException("Authentication failed." + ex.getMessage());
+            throw  new JwtAuthFailException("Authentication failed." + ex.getMessage());
         }
 
     }
@@ -110,6 +116,8 @@ public class AuthenticationService {
     public JwtResponseDto refresh( final String refreshToken    ) {
         return jwtTokenService.refreshUserTokens(refreshToken);
     }
+
+
 
 
 
